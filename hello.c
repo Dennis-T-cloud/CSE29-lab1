0ac169cbad6f6e0d696798a2f8c5377824898826
Dennis-T-cloud <desun@ucsd.edu>
2024-10-01 22:23:24 +0000

    update

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 //A bit change here! :) Dennis
 int main() {
-  char hello[] = "Hello!"; 
+  char hello[] = "Hello! NEW Hello!"; 
   char hello2[] = { 72, 101, 108, 108, 111, 33, 0 };
 
   puts(hello);
